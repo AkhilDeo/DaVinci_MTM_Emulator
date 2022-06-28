a1e5facba690407cc0b779075a8c8725c7265db3
Akhil Deo <43221536+AkhilDeo@users.noreply.github.com>
2022-06-27 22:12:15 -0400

    Updated MyVariables.swift

--- ARPersistence/MyVariables.swift
+++ ARPersistence/MyVariables.swift
@@ -13,4 +13,13 @@ import SceneKit
 struct MyVariables {
     static var ip_address = "0.0.0.0"
     static var network: UDPClient? = nil
+        
+    // for ecm,  joint 1 controls yaw, joint 2 controls pitch, joint 3 controls insertion, and joint 4 controls the roll
+    static var camera_jp: Array = [0.0, 0.0, 0.0, 0.0]
+    static var clutchOffset: Dictionary<String, Float> = ["x": 0.0,
+                                                          "y": 0.0,
+                                                           "z": 0.0,
+                                                          "roll": 0.0,
+                                                          "pitch": 0.0,
+                                                          "yaw": 0.0 ]
 }
